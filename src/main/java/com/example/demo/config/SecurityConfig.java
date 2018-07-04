4266049e2587b40b5acc7e459c275c447bffdb16
Wong <ew.yafei@gmail.com>
2018-07-04 09:02:49 +0800

    project init.

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.example.demo.config;
+
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.http.HttpMethod;
+    import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+    import org.springframework.security.core.userdetails.User;
+    import org.springframework.security.web.authentication.RememberMeServices;
+    import org.springframework.session.FindByIndexNameSessionRepository;
+    import org.springframework.session.Session;
+    import org.springframework.session.security.SpringSessionBackedSessionRegistry;
+    import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
+
+    @EnableWebSecurity
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+//        private FindByIndexNameSessionRepository<Session> sessionRepository;
+        @Autowired
+        FindByIndexNameSessionRepository<? extends Session> sessionRepository;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .sessionManagement()
+                    .maximumSessions(1)
+                    .sessionRegistry(sessionRegistry());
+
+            http
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.GET, "/", "/login").permitAll()
+                    .anyRequest().authenticated()
+
+                    .and()
+                        .formLogin()
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+
+                    .and()
+                        .rememberMe()
+                        .rememberMeServices(rememberMeServices())
+
+                    .and()
+                        .logout()
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+
+                    .and()
+                        .csrf()
+                        .disable();
+        }
+
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser(User.withUsername("user")
+                    .password("{noop}password").roles("USER").build());
+
+        }
+
+        @Bean
+        RememberMeServices rememberMeServices() {
+            SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+            return rememberMeServices;
+        }
+
+        @Bean
+        SpringSessionBackedSessionRegistry sessionRegistry() {
+            return new SpringSessionBackedSessionRegistry<>(this.sessionRepository);
+        }
+    }
---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            model.addAttribute("username", currentUserName);
+        }
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
