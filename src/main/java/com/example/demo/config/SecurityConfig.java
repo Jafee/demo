57e208c61f5d835663142947aa27cf1ac3f22616
Wong <ew.yafei@gmail.com>
2018-07-05 22:31:46 +0800

    update login.html for remember me label.

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,55 +1,57 @@
 package com.example.demo.config;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.http.HttpMethod;
-    import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-    import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-    import org.springframework.security.core.userdetails.User;
-    import org.springframework.security.web.authentication.RememberMeServices;
-    import org.springframework.session.FindByIndexNameSessionRepository;
-    import org.springframework.session.Session;
-    import org.springframework.session.security.SpringSessionBackedSessionRegistry;
-    import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
+import org.springframework.session.security.SpringSessionBackedSessionRegistry;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
-    @EnableWebSecurity
-    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         @Autowired
         FindByIndexNameSessionRepository<? extends Session> sessionRepository;
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .sessionManagement()
-                    .maximumSessions(1)
-                    .sessionRegistry(sessionRegistry());
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .sessionRegistry(sessionRegistry());
 
-            http
-                    .authorizeRequests()
-                    .antMatchers(HttpMethod.GET, "/", "/login").permitAll()
-                    .anyRequest().authenticated()
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/", "/login").permitAll()
+                .anyRequest().authenticated()
 
-                    .and()
-                        .formLogin()
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/")
 
-                    .and()
-                        .rememberMe()
-                        .rememberMeServices(rememberMeServices())
+                .and()
+                    .rememberMe()
+                    .rememberMeParameter("remember-me")
+                    .rememberMeCookieName("remember-me")
+                    .rememberMeServices(rememberMeServices())
 
-                    .and()
-                        .logout()
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
 
-                    .and()
-                        .csrf()
-                        .disable();
+                .and()
+                    .csrf()
+                    .disable();
         }
 
 
